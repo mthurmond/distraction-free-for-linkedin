0f87b9b2d31b1ece612843c008b2afcfcdbaabad
Matt Thurmond <matt.thurmond@gmail.com>
2021-11-14 12:24:43 -0500

    finish mvp

--- extension/hider/hider.js
+++ extension/hider/hider.js
@@ -5,22 +5,3 @@ stylesheetElement.rel = 'stylesheet';
 stylesheetElement.setAttribute('href', stylesheetUrl);
 stylesheetElement.setAttribute('id', "hider__main-stylesheet");
 document.body.appendChild(stylesheetElement);
-
-// //once the required elements exist, this function initiates the js
-// function initiateDistractionFreeLinkedin() {
-//     addToggleButton();
-    
-//     //call this to hide messages by default when slack is first loaded
-//     toggleMessages(showMessages);
-// }
-
-// //continuously check if the required elements exist. once they do, stop checking and call the appropriate function. 
-// const checkForElements = setInterval(function () {
-//     if (
-//         document.getElementsByClassName('feed-shared-actor').length > 0 
-//         && document.title.length > 0
-//     ) {
-//         clearInterval(checkForElements);
-//         initiateDistractionFreeLinkedin();
-//     }
-// }, 100);
