6f3c8da80602f827768a8290e06dd02d3aeddbfe
Matt Thurmond <matt.thurmond@gmail.com>
2021-11-14 19:23:53 -0500

    remove element flicker on page load

--- extension/hider/hider.js
+++ extension/hider/hider.js
@@ -1,7 +1,15 @@
-// add stylesheet to the DOM so the "message hidden" styles appear immediately
-const stylesheetUrl = chrome.runtime.getURL('hider/hider-main.css');    
-const stylesheetElement = document.createElement('link');
-stylesheetElement.rel = 'stylesheet';
-stylesheetElement.setAttribute('href', stylesheetUrl);
-stylesheetElement.setAttribute('id', "hider__main-stylesheet");
-document.body.appendChild(stylesheetElement);
+const checkForBody = setInterval(function () {
+
+    if (
+        document.body
+    ) {
+        clearInterval(checkForBody);
+        const stylesheetUrl = chrome.runtime.getURL('hider/hider-main.css');    
+        const stylesheetElement = document.createElement('link');
+        stylesheetElement.rel = 'stylesheet';
+        stylesheetElement.setAttribute('href', stylesheetUrl);
+        stylesheetElement.setAttribute('id', "hider__main-stylesheet");
+        document.body.appendChild(stylesheetElement);
+    }
+
+}, 100);
