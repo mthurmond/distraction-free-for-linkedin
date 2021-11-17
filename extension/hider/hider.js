a83ea4d567a3d14185a4490063b912dabef12e8c
Matt Thurmond <matt.thurmond@gmail.com>
2021-11-16 20:49:23 -0500

    remove messages from page title

--- extension/hider/hider.js
+++ extension/hider/hider.js
@@ -1,8 +1,7 @@
 const checkForBody = setInterval(function () {
 
-    if (
-        document.body
-    ) {
+    if (document.body) {
+
         clearInterval(checkForBody);
         const stylesheetUrl = chrome.runtime.getURL('hider/hider-main.css');    
         const stylesheetElement = document.createElement('link');
@@ -10,6 +9,31 @@ const checkForBody = setInterval(function () {
         stylesheetElement.setAttribute('href', stylesheetUrl);
         stylesheetElement.setAttribute('id', "hider__main-stylesheet");
         document.body.appendChild(stylesheetElement);
+
+    }
+
+}, 100);
+
+const checkForTitle = setInterval(function () {
+
+    if (
+        document.title
+    ) {
+
+        document.title = 'LinkedIn';
+
+        //activate the mutation observer
+        titleObserver = new MutationObserver(function(mutations) {
+            if (document.title != 'LinkedIn') {
+                document.title = 'LinkedIn';
+            } 
+        });
+    
+        titleObserver.observe(
+            document.querySelector('title'),
+            { characterData: true, childList: true }
+        );
+
     }
 
 }, 100);
